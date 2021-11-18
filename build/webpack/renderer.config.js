c9b47a790ddc41ceac57d8546c0dd6b329900e17
陈子超 <chenzichao@ydjdev.com>
2021-11-18 16:21:14 +0800

    feat: eslint&prettier配置

--- build/webpack/renderer.config.js
+++ build/webpack/renderer.config.js
@@ -1,55 +1,60 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
+const config = {
   mode: process.env.NODE_ENV,
-  entry: { bundle: ['./src/renderer/index.tsx'] },
+  entry: { bundle: ["./src/renderer/index.tsx"] },
   output: {
     // todo: change path and name according to env environment
     hashDigestLength: 10,
-    filename: 'js/web-[name].js',
-    path: path.resolve(__dirname, '../../', 'dist'),
-    publicPath: '/',
-    chunkFilename: 'js/web-[name].js',
-    globalObject: `(typeof self !== 'undefined' ? self : this)`
+    filename: "js/web-[name].js",
+    path: path.resolve(__dirname, "../../", "dist"),
+    publicPath: "/",
+    chunkFilename: "js/web-[name].js",
+    globalObject: `(typeof self !== 'undefined' ? self : this)`,
   },
 
-  devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
-    hot: true,
-    historyApiFallback: true,
-    compress: true,
-  },
   resolve: {
     alias: {
-      '@': path.resolve('src')
+      "@": path.resolve("src"),
     },
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: [".ts", ".tsx", ".js", ".json"],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         use: {
-          loader: require.resolve('babel-loader')
+          loader: require.resolve("babel-loader"),
         },
         exclude: [/node_modules/],
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-        filename: 'index.html',
-      template: 'build/webpack/tpl/index.html',
+      filename: "index.html",
+      template: "build/webpack/tpl/index.html",
       inject: true,
       minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeAttributeQuotes: true
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
       },
-      chunksSortMode: 'dependency'
-  }),
-  ]
+      chunksSortMode: "dependency",
+    }),
+  ],
 };
+
+if (process.env.NODE_ENV === "development") {
+  config.devServer = {
+    contentBase: path.resolve(__dirname, "dist"),
+    hot: true,
+    historyApiFallback: true,
+    compress: true,
+  };
+}
+
+module.exports = config;
